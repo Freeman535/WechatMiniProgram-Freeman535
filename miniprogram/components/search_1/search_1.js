76871fd4423308056ef52524b0d2c1a04c822185
freem <1>
2021-09-30 17:25:28 +0800

    0930-01-1

--- miniprogram/components/search_1/search_1.js
+++ miniprogram/components/search_1/search_1.js
@@ -1,4 +1,10 @@
 // components/search_1/search_1.js
+const app = getApp()
+
+// 此组件应用需传入
+// 门店列表和开始日期 
+// CanSearch 和 back
+
 Component({
   /**
    * 组件的属性列表
@@ -6,12 +12,38 @@ Component({
   properties: {
     canSearch: {
       type: Number,
-      value: 1
+      value: 0
     },
     back: {
       type: Number,
       value: 4
     },
+    nowdate:{
+      type:String,
+      
+    },
+    useS_RQ: {
+      type:Boolean,
+    },
+    useE_RQ: {
+      type:Boolean,
+    },
+    useDH: {
+      type:Boolean,
+    },
+    use69CODE: {
+      type:Boolean,
+    },
+    useCODE: {
+      type:Boolean,
+    },
+    useNAME: {
+      type:Boolean,
+    },
+    fdarray:{
+      type:Array,
+    }
+
     
   
   },
@@ -20,14 +52,34 @@ Component({
    * 组件的初始数据
    */
   data: {
-
-  
+    showText:[0, '2021-09-30', '', '', '', '', ''],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    reSet(){
+      var tempShow = this.data.showText
+      tempShow[1] = this.data.nowdate
+      this.setData({
+        showText: tempShow
+      })
+      this.triggerEvent("reSetComponent")
+    },
 
+    bindChange(e){
+      // console.log(e.detail.value) 返回输入值
+      // console.log(e.target.dataset.id) 返回id
+      var tempShow = this.data.showText
+      tempShow[e.target.dataset.id] = e.detail.value
+      this.setData({
+        showText: tempShow
+      })
+    },
+
+    bindSearch(){
+      console.log(this.data.showText)
+    }
   }
 })
--- miniprogram/pages/personal/index.js
+++ miniprogram/pages/personal/index.js
@@ -22,8 +22,9 @@ Page({
     wx.hideHomeButton({
       success: (res) => {},
     })
+    app.globalData.NowDate = this.getNowFormatDate()
     this.setData({
-      ud:app.globalData['userData']['PROP']
+      ud:app.globalData['userData']['PROP'],
     })
   },
 
@@ -33,6 +34,22 @@ Page({
   onReady: function () {
 
   },
+  getNowFormatDate: function() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+  },
+
   GoTo: function(e){
     console.log(e)
     if (e.target['dataset']['prop'] == 'oy'){
